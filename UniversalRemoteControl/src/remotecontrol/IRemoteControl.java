afd51621f791ded2c68a8a6bab7963973a2785e8
not7cd <norbert@not7cd.net>
2021-11-04 11:25:34 +0100

    Reformat

--- UniversalRemoteControl/src/remotecontrol/IRemoteControl.java
+++ UniversalRemoteControl/src/remotecontrol/IRemoteControl.java
@@ -6,6 +6,7 @@ package remotecontrol;
 public interface IRemoteControl {
 
     // TODO: Method for configuration of action buttons
+    public void configureButton(int no, int undoNo, String action);
 
     /**
      * The action button was pressed.
